0150ab8b856dc7957ce35e1bd2db27ff5293be9d
l3lackrooz <53308699+l3lackrooz@users.noreply.github.com>
2021-08-12 14:30:02 +0300

    Update createAddress.php

--- BoxPayment/createAddress.php
+++ BoxPayment/createAddress.php
@@ -1,18 +1,28 @@
 <?php
 
 namespace BoxPayment;
+use use Illuminate\Support\Facades\Http;
 
 class CreateAddress
 {
-    private $length = 8;
+    private $api_key;
+    private $amount;
+    private $token;
+    private $callback;
 
-    public function __construct($length)
+    public function __construct($api_key,$amount,$token,$callback)
     {
-        $this->length = $length;
+        $this->api_key = $length;
     }
 
     public function generate()
     {
+        $array = [
+            "amount"=>"10",
+            "token"=>"USDT",
+            "callback"=>"http://localhost/callback.com",
+            "api_key"=>"MlKDEG9HcfNlnMUeTLnmWACbgs9RuAl38pkRSVJckKZrkKYFG8pFrWKJU3OGzuPW"
+        ];
         $alphabet = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789[{(*%+-$#@!)}]";
         $pass = [];
         $alphaLength = strlen($alphabet) - 1;
