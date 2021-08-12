002a5d3223f7445a4ffd7856f2d9d16646106dec
l3lackrooz <53308699+l3lackrooz@users.noreply.github.com>
2021-08-12 16:50:17 +0300

    Create config.php

---
+++ config/config.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Box Payment 
+    |--------------------------------------------------------------------------
+    |
+    | You can configure the gateway settings from here.
+    |
+    */
+
+    'dashboard' => [
+
+        'api_key' => env('BOX_PAYMENT_API_KEY'),
+
+        'iv' => env('BOX-PAYMENT_IV'),
+
+    ],
