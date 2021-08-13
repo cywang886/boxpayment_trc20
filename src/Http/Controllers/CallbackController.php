eb452050e49ca604502941c74c1866cb06abe7bd
l3lackrooz <53308699+l3lackrooz@users.noreply.github.com>
2021-08-13 20:30:31 +0300

    Create CallbackController.php

---
+++ src/Http/Controllers/CallbackController.php
@@ -0,0 +1,33 @@
+<?php
+namespace boxpayment\laravel\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use boxpayment\laravel\Http\Middleware\VerifySignature;
+
+class WebhookController extends Controller
+{
+  public function __construct()
+    {
+        $this->middleware(VerifySignature::class);
+    }
+  public function __invoke(Request $request)
+    {
+//         $payload = $request->input();
+
+//         $model = config('coinbase.webhookModel');
+
+//         $coinbaseWebhookCall = $model::create([
+//             'type' =>  $payload['event']['type'] ?? '',
+//             'payload' => $payload,
+//         ]);
+
+//         try {
+//             $coinbaseWebhookCall->process();
+//         } catch (\Exception $e) {
+//             $coinbaseWebhookCall->saveException($e);
+
+//             throw $e;
+//         }
+    }
+}
