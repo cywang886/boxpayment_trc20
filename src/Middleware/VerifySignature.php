5d97baf896f6b22c0f6aa8a086c1bd1e380f699a
l3lackrooz <53308699+l3lackrooz@users.noreply.github.com>
2021-08-13 20:24:01 +0300

    Update VerifySignature.php

--- src/Middleware/VerifySignature.php
+++ src/Middleware/VerifySignature.php
@@ -6,5 +6,24 @@ use Closure
 class VerifySignature
 {
   public function handle($request, Closure $next){
+    
+    $signature = $request->sign;
+    
+    if (! $signature) {
+      return ['success' => false];
+    }
+
+    if (! $this->isValid($signature, $request->all())) {
+      return ['success' => false];
+    }
+    
+    return $next($request);
   }
+  
+  protected function isValid(string $signature, string $payload): bool
+    
+    $secret = config('boxpayment.apiKey');
+    if (empty($secret)) {
+      return false;
+    } 
 }
