55ec1ec676ab2d9bdf681977995318d35dd16436
l3lackrooz <53308699+l3lackrooz@users.noreply.github.com>
2021-08-13 20:05:17 +0300

    Create api.php

---
+++ src/Routes/api.php
@@ -0,0 +1,5 @@
+<?php
+
+Route::group(['prefix' => 'api',  'middleware' => 'api'], function() {
+    Route::post('boxpayment/callback', '\boxpayment\laravel\Http\Controllers\CallbackController')->name('boxpayment-callback');
+});
