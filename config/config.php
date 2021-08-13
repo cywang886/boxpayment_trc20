13687a8ae778c04235882a49b7aa8191f5d74f06
l3lackrooz <53308699+l3lackrooz@users.noreply.github.com>
2021-08-13 20:26:01 +0300

    Update config.php

--- config/config.php
+++ config/config.php
@@ -10,12 +10,8 @@ return [
     | You can configure the gateway settings from here.
     |
     */
-
-    'dashboard' => [
-
-        'api_key' => env('BOX_PAYMENT_API_KEY'),
-
-        'iv' => env('BOX-PAYMENT_IV'),
-
-    ],
+    'base_url' => env('BOX_PAYMENT_BASE_URL'),
+    'callback_uri' => env('BOX_PAYMENT_CALLBACK_URI'),
+    'api_key' => env('BOX_PAYMENT_API_KEY'),
+    'iv' => env('BOX-PAYMENT_IV'),
 ];
