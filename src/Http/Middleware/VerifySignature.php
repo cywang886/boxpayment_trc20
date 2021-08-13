a3a8a956646bc7d12c84cc06c421358a82a10427
l3lackrooz <53308699+l3lackrooz@users.noreply.github.com>
2021-08-13 21:54:08 +0300

    Create VerifySignature.php

---
+++ src/Http/Middleware/VerifySignature.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Boxpayment\Laravel\Http\Middleware;
+
+use Closure;
+
+class VerifySignature
+{
+  public function handle($request, Closure $next)
+  {
+
+    $signature = $request->sign;
+
+    if (!$signature) {
+      return ['success' => false];
+    }
+
+    if (!$this->isValid($signature, $request->all())) {
+      return ['success' => false];
+    }
+
+    return $next($request);
+  }
+
+  protected function isValid(string $signature, string $payload): bool
+  {
+    $secret = config('boxpayment.apiKey');
+    if (empty($secret)) {
+      return false;
+    }
+  }
+}
