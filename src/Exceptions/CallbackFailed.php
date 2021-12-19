e1a9dc95682a6df48bf19c0f45c7163d93a99823
dan <l3lackrooz@gmail.com>
2021-12-19 06:28:24 +0300

    add comment

--- src/Exceptions/CallbackFailed.php
+++ src/Exceptions/CallbackFailed.php
@@ -7,24 +7,43 @@ use Illuminate\Http\Request;
 
 class CallbackFailed extends Exception
 {
+    /**
+     * missingSignature
+     *
+     * @return void
+     */
     public static function missingSignature()
     {
         return new static('The request did not contain a `sign` argumant.');
     }
 
+    /**
+     * invalidRequest
+     *
+     * @return void
+     */
     public static function invalidRequest()
     {
         return new static("The Request not valid.");
     }
 
+    /**
+     * sharedSecretNotSet
+     *
+     * @return void
+     */
     public static function sharedSecretNotSet()
     {
         return new static('The Boxpayment callback shared secret is not set. Make sure that the `boxpayment.apiKey` config key is set to the value you found on the BoxPayment Admin dashboard.');
     }
 
+    /**
+     * sharedIVNotSet
+     *
+     * @return void
+     */
     public static function sharedIVNotSet()
     {
         return new static('The Boxpayment callback IV is not set. Make sure that the `boxpayment.iv` config key is set to the value you found on the BoxPayment Admin dashboard.');
     }
-
 }
